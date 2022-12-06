57d702e743d24c559e0d8fc5a5ab738555103eff
riniweb <119380064+riniweb@users.noreply.github.com>
2022-12-06 13:20:32 -0600

    added the relevant icon from the api

--- vanilla-weather-app/src/app.js
+++ vanilla-weather-app/src/app.js
@@ -15,19 +15,22 @@ function formatDate(timestamp){
 
 
 function displayTemperature(response){
-  
+  console.log(response.data)
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let speedElement = document.querySelector("#speed")
     let dateElement = document.querySelector("#date")
+    let iconElement = document.querySelector("#icon")
     temperatureElement.innerHTML = Math.round(response.data.temperature.current);
     cityElement.innerHTML = response.data.city;
    descriptionElement.innerHTML = response.data.condition.description;
    humidityElement.innerHTML = response.data.temperature.humidity;
    speedElement.innerHTML = Math.round(response.data.wind.speed);
    dateElement.innerHTML = formatDate(response.data.time * 1000);
+   iconElement.setAttribute("src",`http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png` );  
+   iconElement.setAttribute("alt", response.data.condition.description);
 }
 
 

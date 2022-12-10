b85cb1b61c8811055b53e6bfde2f3e692f8f4653
riniweb <119380064+riniweb@users.noreply.github.com>
2022-12-10 12:51:50 -0600

    Implemented the unit conversion

--- vanilla-weather-app/src/app.js
+++ vanilla-weather-app/src/app.js
@@ -23,7 +23,10 @@ function displayTemperature(response){
     let speedElement = document.querySelector("#speed")
     let dateElement = document.querySelector("#date")
     let iconElement = document.querySelector("#icon")
-    temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+
+    celsiusTemperature = response.data.temperature.current;
+
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
     cityElement.innerHTML = response.data.city;
    descriptionElement.innerHTML = response.data.condition.description;
    humidityElement.innerHTML = response.data.temperature.humidity;
@@ -48,10 +51,35 @@ function handleSubmit(event){
 }
  
 
-search("Chicago");
+function displayFahrenheitTemperature(event){
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+
+    celsiusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
+    let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+    temperatureElement.innerHTML= Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+    event.preventDefault();
+    celsiusLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+    
+let celsiusTemperature = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("Chicago");
 
 

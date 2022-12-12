68c40b2cf72a546c5871d0d30459dd5570cef837
riniweb <119380064+riniweb@users.noreply.github.com>
2022-12-12 13:51:07 -0600

    created time stamps using the api

--- src/app.js
+++ src/app.js
@@ -13,31 +13,41 @@ function formatDate(timestamp){
     return `${day} ${hours} : ${minutes}`;
 }
 
+function formatDay(timestamp){
+let date = new Date(timestamp * 1000);
+let day = date.getDay();
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+return days[day]; 
+}
+
 function displayForecast(response){
+    let forecast = response.data.daily;
    
 let forecastElement = document.querySelector("#forecast");
 
     let forecastHTML = `<div class="row"`;
-    let days = ["Thu", "Fri", "Sat","Sun"];
-    days.forEach(function (day) {
-
+    
+   forecast.forEach(function (forecastDay, index) {
+       if (index < 6){   
         forecastHTML = 
         forecastHTML + 
         ` 
         <div class="col-2">
-          <div class="weather-forecast-date">${day}</div>
-          <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png" 
+          <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+          <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${forecastDay.weather.icon}.png" 
           alt=""
           width="42"/>
           <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperatures-max"> 18° </
+          <span class="weather-forecast-temperatures-max"> ${Math.round(forecastDay.temperature.max)}° </
           span>
             <span class="weather-forecast-temperatures-min">
-             12°
+             ${Math.round(forecastDay.temperature.min)}°
             </span>
           </div>
         </div>
       `;
+      }
     });
         forecastHTML = forecastHTML + `</div`;   
         forecastElement.innerHTML = forecastHTML
